6f17e6e5fcd69e488a94fad6b5937018b706fe2a
ahmedaly113 <60133587+ahmedaly113@users.noreply.github.com>
2021-03-22 18:08:59 +0800

    Update x/mint/abci.go
    Co-authored-by: Dev Ojha <ValarDragon@users.noreply.github.com>

--- x/mint/abci.go
+++ x/mint/abci.go
@@ -27,7 +27,10 @@ func BeginBlocker(ctx sdk.Context, k keeper.Keeper) {
 	minter := k.GetMinter(ctx)
 	params := k.GetParams(ctx)
 
-	if k.GetEpochNum(ctx) >= int64(k.GetParams(ctx).HalvenPeriodInEpoch)+k.GetLastHalvenEpochNum(ctx) {
+        // Check if we have hit an epoch where we update the inflation parameter.
+        // Since epochs only update based on BFT time data, it is safe to store the "halvening period time" 
+        // in terms of the number of epochs that have transpired. 
+     	if k.GetEpochNum(ctx) >= int64(k.GetParams(ctx).HalvenPeriodInEpoch)+k.GetLastHalvenEpochNum(ctx) {
 		// Halven the reward per halven period
 		minter.AnnualProvisions = minter.NextAnnualProvisions(params)
 		k.SetMinter(ctx, minter)
