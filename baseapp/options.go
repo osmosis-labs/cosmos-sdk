9b9f0acedebb20b684b08cfe532115970aa690f7
marbar3778 <marbar3778@yahoo.com>
2022-02-15 11:44:48 +0100

    fix comment

--- baseapp/options.go
+++ baseapp/options.go
@@ -93,7 +93,7 @@ func (app *BaseApp) SetParamStore(ps ParamStore) {
 	app.paramStore = ps
 }
 
-// SetVersion sets the application's version string. TODO
+// SetVersion sets the application's version string.
 func (app *BaseApp) SetVersion(v string) {
 	if app.sealed {
 		panic("SetVersion() on sealed BaseApp")
@@ -109,7 +109,7 @@ func (app *BaseApp) SetProtocolVersion(v uint64) {
 	app.paramStore.Set(app.deliverState.ctx, ParamStoreKeyVersionParams, av)
 }
 
-// SetProtocolVersion sets the application's protocol version
+// GetProtocolVersion gets the application's protocol version
 func (app *BaseApp) GetProtocolVersion() uint64 {
 
 	av := &tmproto.VersionParams{}
