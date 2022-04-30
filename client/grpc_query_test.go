ffbc01a77c0c8751271bd25a3660b895ec4781f4
Roman <ackhtariev@gmail.com>
2022-04-30 15:46:47 -0400

    table tests for selectHeight

--- client/grpc_query_test.go
+++ client/grpc_query_test.go
@@ -1,5 +1,3 @@
-// +build norace
-
 package client_test
 
 import (
@@ -7,10 +5,12 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
 
+	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/testutil/network"
 	"github.com/cosmos/cosmos-sdk/testutil/testdata"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -79,4 +79,55 @@ func (s *IntegrationTestSuite) TestGRPCQuery() {
 
 func TestIntegrationTestSuite(t *testing.T) {
 	suite.Run(t, new(IntegrationTestSuite))
+}
+
+func TestSelectHeight(t *testing.T) {
+	// if height is set to this, the tests assume that it is not set
+	const heightNotSetFlag = int64(-1)
+
+	testcases := map[string]struct {
+		clientContextHeight int64
+		grpcHeight          int64
+		expectedHeight      int64
+	}{
+		"clientContextHeight 1; grpcHeight not set - clientContextHeight selected": {
+			clientContextHeight: 1,
+			grpcHeight:          heightNotSetFlag,
+			expectedHeight:      1,
+		},
+		"clientContextHeight not set; grpcHeight is 2 - grpcHeight is chosen": {
+			clientContextHeight: heightNotSetFlag,
+			grpcHeight:          2,
+			expectedHeight:      2,
+		},
+		"both not set - 0 returned": {
+			clientContextHeight: heightNotSetFlag,
+			grpcHeight:          heightNotSetFlag,
+			expectedHeight:      0,
+		},
+		"clientContextHeight 3; grpcHeight is 0 - grpcHeight is chosen": {
+			clientContextHeight: 3,
+			grpcHeight:          0,
+			expectedHeight:      0,
+		},
+	}
+
+	for name, tc := range testcases {
+		t.Run(name, func(t *testing.T) {
+			clientCtx := client.Context{}
+			if tc.clientContextHeight != heightNotSetFlag {
+				clientCtx = clientCtx.WithHeight(tc.clientContextHeight)
+			}
+
+			grpcContxt := context.Background()
+			if tc.grpcHeight != heightNotSetFlag {
+				header := metadata.Pairs(grpctypes.GRPCBlockHeightHeader, fmt.Sprintf("%d", tc.grpcHeight))
+				grpcContxt = metadata.NewOutgoingContext(grpcContxt, header)
+			}
+
+			height, err := client.SelectHeight(clientCtx, grpcContxt)
+			require.NoError(t, err)
+			require.Equal(t, tc.expectedHeight, height)
+		})
+	}
 }
