bb864db59950869e5cb7febbba1d7d1d16d4b7bb
Roman <roman@osmosis.team>
2022-05-23 08:41:48 -0700

    Update x/upgrade/exported/exported.go
    Co-authored-by: Aleksandr Bezobchuk <alexanderbez@users.noreply.github.com>

--- x/upgrade/exported/exported.go
+++ x/upgrade/exported/exported.go
@@ -2,8 +2,7 @@ package exported
 
 import sdk "github.com/cosmos/cosmos-sdk/types"
 
-// ProtocolVersionManager defines the interface
-// which allows managing it's appVersion field.
+// ProtocolVersionManager defines the interface which allows managing the appVersion field.
 type ProtocolVersionManager interface {
 	GetProtocolVersion(ctx sdk.Context) (uint64, error)
 	SetProtocolVersion(ctx sdk.Context, version uint64) error
