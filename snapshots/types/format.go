16d062d67a9255cc503a1ec4596084fd12f3cb62
Roman <ackhtariev@gmail.com>
2022-06-15 23:00:10 -0400

    update godoc for CurrentFormat

--- snapshots/types/format.go
+++ snapshots/types/format.go
@@ -1,9 +1,10 @@
 package types
 
-// CurrentFormat is the currently used format for snapshots. Snapshots using the same format
-// must be identical across all nodes for a given height, so this must be bumped when the binary
+// CurrentFormat is the currently used format for snapshots.
+// Essentially, it versions the format of the snapshot data.
+// Snapshots using the same format must be identical across all
+// nodes for a given height, so this must be bumped when the binary
 // snapshot output changes.
-// Format 1 was the original format.
-// Format 2 serailizes app version in addition to the original 
-// snapshot data.
+// CurrentFormat of 1 is the original format.
+// CurrentFormat of 2 serailizes app version in addition to the original snapshot data.
 const CurrentFormat uint32 = 2
