c341c969f000e0c9a1b0c436ee7504f510d13be8
Sunny Aggarwal <sunnya97@protonmail.ch>
2022-06-30 18:52:48 -0400

    edit comment

--- x/bank/types/errors.go
+++ x/bank/types/errors.go
@@ -6,7 +6,7 @@ import (
 
 // x/bank module sentinel errors
 var (
-	// Error codes 2-4 were already used and removed, but should not be recycled.
+	// Error codes 2-4 were already assigned and removed, but should not be recycled.
 	ErrSendDisabled          = sdkerrors.Register(ModuleName, 5, "send transactions are disabled")
 	ErrDenomMetadataNotFound = sdkerrors.Register(ModuleName, 6, "client denom metadata not found")
 	ErrInvalidKey            = sdkerrors.Register(ModuleName, 7, "invalid key")
