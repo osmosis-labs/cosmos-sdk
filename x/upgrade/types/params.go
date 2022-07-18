ba6148f856ba985eb0b9e2f8a16d4608a9d54265
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2022-07-18 11:24:53 +0800

    updates

---
+++ x/upgrade/types/params.go
@@ -0,0 +1,43 @@
+package types
+
+import (
+	fmt "fmt"
+
+	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
+)
+
+// Parameter store keys
+var (
+	KeyIsMainnet = []byte("IsMainnet")
+)
+
+// ParamKeyTable returns a KeyTable for the x/upgrade module.
+func ParamKeyTable() paramtypes.KeyTable {
+	return paramtypes.NewKeyTable().RegisterParamSet(&Params{})
+}
+
+func NewParams(isMainnet bool) Params {
+	return Params{
+		IsMainnet: isMainnet,
+	}
+}
+
+func DefaultParams() Params {
+	return NewParams(true)
+}
+
+// ParamSetPairs returns the ParamSetPairs for the x/upgrade module.
+func (p *Params) ParamSetPairs() paramtypes.ParamSetPairs {
+	return paramtypes.ParamSetPairs{
+		paramtypes.NewParamSetPair(KeyIsMainnet, &p.IsMainnet, validateIsMainnet),
+	}
+}
+
+func validateIsMainnet(i interface{}) error {
+	_, ok := i.(bool)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", i)
+	}
+
+	return nil
+}
