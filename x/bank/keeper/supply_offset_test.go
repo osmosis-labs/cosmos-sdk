ea94e528c300697270b55bc5998ca310289e29a2
Sunny Aggarwal <sunnya97@protonmail.ch>
2022-07-24 17:32:20 +0200

    in progress

--- x/bank/keeper/supply_offset_test.go
+++ x/bank/keeper/supply_offset_test.go
@@ -9,46 +9,74 @@ import (
 )
 
 func (suite *IntegrationTestSuite) TestAddSupplyOffset() {
-	app, ctx, queryClient := suite.app, suite.ctx, suite.queryClient
 
-	test1Supply := sdk.NewInt64Coin("test1", 4000000)
-	expectedTotalSupply := sdk.NewCoins(test1Supply)
-	suite.
-		Require().
-		NoError(app.BankKeeper.MintCoins(ctx, minttypes.ModuleName, expectedTotalSupply))
+	testCases := []struct {
+		description        string
+		initialSupply      sdk.Int
+		supplyOffsetAmount sdk.Int
+		expPass            bool
+		supplyWithOffset   sdk.Int
+	}{
+		{
+			description:        "test positive supply offset",
+			initialSupply:      sdk.NewInt(10000),
+			supplyOffsetAmount: sdk.NewInt(1000),
+			expPass:            true,
+			supplyWithOffset:   sdk.NewInt(11000),
+		},
+		{
+			description:        "test negative supply offset",
+			initialSupply:      sdk.NewInt(10000),
+			supplyOffsetAmount: sdk.NewInt(-1000),
+			expPass:            true,
+			supplyWithOffset:   sdk.NewInt(9000),
+		},
+		{
+			description:        "make sure that supplyWithOffset can't go negative",
+			initialSupply:      sdk.NewInt(10000),
+			supplyOffsetAmount: sdk.NewInt(-11000),
+			expPass:            false,
+			supplyWithOffset:   sdk.NewInt(10000),
+		},
+	}
 
-	_, err := queryClient.SupplyOf(gocontext.Background(), &types.QuerySupplyOfRequest{})
-	suite.Require().Error(err)
+	for _, tc := range testCases {
+		tc := tc
 
-	res, err := queryClient.SupplyOf(gocontext.Background(), &types.QuerySupplyOfRequest{Denom: test1Supply.Denom})
-	suite.Require().NoError(err)
-	suite.Require().NotNil(res)
+		suite.Run(tc.description, func() {
+			app, ctx, queryClient := suite.app, suite.ctx, suite.queryClient
 
-	suite.Require().Equal(test1Supply, res.Amount)
+			// mint the initialSupply coins
+			test1Supply := sdk.NewCoin("test1", tc.initialSupply)
+			suite.
+				Require().
+				NoError(app.BankKeeper.MintCoins(ctx, minttypes.ModuleName, sdk.NewCoins(test1Supply)))
 
-	// test total supply of query with supply offset
-	err = app.BankKeeper.AddSupplyOffset(ctx, "test1", sdk.NewInt(-1000000))
-	suite.Require().NoError(err)
-	res, err = queryClient.SupplyOf(gocontext.Background(), &types.QuerySupplyOfRequest{Denom: test1Supply.Denom})
-	suite.Require().NoError(err)
-	suite.Require().NotNil(res)
+			// make sure that queried supply is correct
+			res, err := queryClient.SupplyOf(gocontext.Background(), &types.QuerySupplyOfRequest{Denom: test1Supply.Denom})
+			suite.Require().NoError(err)
+			suite.Require().NotNil(res)
+			suite.Require().Equal(test1Supply, res.Amount)
 
-	suite.Require().Equal(test1Supply.Sub(sdk.NewInt64Coin("test1", 1000000)), res.Amount)
+			// try adding the supply offset
+			err = app.BankKeeper.AddSupplyOffset(ctx, "test1", tc.supplyOffsetAmount)
+			if tc.expPass {
+				suite.Require().NoError(err)
+			} else {
+				suite.Require().Error(err)
+			}
 
-	// make sure query without offsets hasn't changed
-	res2, err := queryClient.SupplyOfWithoutOffset(gocontext.Background(), &types.QuerySupplyOfWithoutOffsetRequest{Denom: test1Supply.Denom})
-	suite.Require().NoError(err)
-	suite.Require().NotNil(res2)
+			// test querying supply results in expected change
+			res, err = queryClient.SupplyOf(gocontext.Background(), &types.QuerySupplyOfRequest{Denom: test1Supply.Denom})
+			suite.Require().NoError(err)
+			suite.Require().NotNil(res)
+			suite.Require().Equal(tc.supplyWithOffset, res.Amount.Amount)
 
-	suite.Require().Equal(test1Supply, res2.Amount)
-
-	// make sure that the supplyWithoutOffset can't go negative
-	err = app.BankKeeper.AddSupplyOffset(ctx, "test1", sdk.NewInt(-4000000))
-	suite.Require().Error(err)
-
-	// make sure supply didn't change
-	res2, err = queryClient.SupplyOfWithoutOffset(gocontext.Background(), &types.QuerySupplyOfWithoutOffsetRequest{Denom: test1Supply.Denom})
-	suite.Require().NoError(err)
-	suite.Require().NotNil(res2)
-	suite.Require().Equal(test1Supply, res2.Amount)
+			// test querying supply without offset did not change
+			res2, err := queryClient.SupplyOfWithoutOffset(gocontext.Background(), &types.QuerySupplyOfWithoutOffsetRequest{Denom: test1Supply.Denom})
+			suite.Require().NoError(err)
+			suite.Require().NotNil(res2)
+			suite.Require().Equal(test1Supply, res2.Amount)
+		})
+	}
 }
