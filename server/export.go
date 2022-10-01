4153bd73cc825e37314f8af686c5acc458e001d1
Adam Tucker <adam@osmosis.team>
2022-10-01 00:18:05 -0500

    change default to nil

--- server/export.go
+++ server/export.go
@@ -118,7 +118,7 @@ func ExportCmd(appExporter types.AppExporter, defaultNodeHome string) *cobra.Com
 	cmd.Flags().Int64(FlagHeight, -1, "Export state from a particular height (-1 means latest height)")
 	cmd.Flags().Bool(FlagForZeroHeight, false, "Export state to start at height zero (perform preproccessing)")
 	cmd.Flags().StringSlice(FlagJailAllowedAddrs, []string{}, "Comma-separated list of operator addresses of jailed validators to unjail")
-	cmd.Flags().StringSlice(FlagModulesToExport, []string{}, "Comma-separated list of modules to export. If empty, will export all modules")
+	cmd.Flags().StringSlice(FlagModulesToExport, nil, "Comma-separated list of modules to export. If empty, will export all modules")
 
 	return cmd
 }
