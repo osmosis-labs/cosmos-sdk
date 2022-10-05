9464a73dc3f65ef2068af09e86133b2c0fc87975
Dev Ojha <dojha@berkeley.edu>
2022-10-05 14:27:54 +0200

    remove extra proto local registers

--- x/gov/types/codec.go
+++ x/gov/types/codec.go
@@ -11,7 +11,6 @@ import (
 // RegisterLegacyAminoCodec registers all the necessary types and interfaces for the
 // governance module.
 func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
-	RegisterProtoLocallyIfNotRegistered()
 	cdc.RegisterInterface((*Content)(nil), nil)
 	cdc.RegisterConcrete(&MsgSubmitProposal{}, "cosmos-sdk/MsgSubmitProposal", nil)
 	cdc.RegisterConcrete(&MsgDeposit{}, "cosmos-sdk/MsgDeposit", nil)
@@ -21,7 +20,6 @@ func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
 }
 
 func RegisterInterfaces(registry types.InterfaceRegistry) {
-	RegisterProtoLocallyIfNotRegistered()
 	registry.RegisterImplementations((*sdk.Msg)(nil),
 		&MsgSubmitProposal{},
 		&MsgVote{},
