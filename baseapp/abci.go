dacf439c705ac7b1cec503b8d4232c2acb00928e
Adam Tucker <adamleetucker@outlook.com>
2022-10-08 21:43:57 -0500

    add const

--- baseapp/abci.go
+++ baseapp/abci.go
@@ -808,6 +808,7 @@ func handleQueryApp(app *BaseApp, path []string, req abci.RequestQuery) abci.Res
 }
 
 func handleQueryStore(app *BaseApp, path []string, req abci.RequestQuery) abci.ResponseQuery {
+	const storeInfoPath = "info"
 	// "/store" prefix for store queries
 	queryable, ok := app.cms.(sdk.Queryable)
 	if !ok {
@@ -828,7 +829,7 @@ func handleQueryStore(app *BaseApp, path []string, req abci.RequestQuery) abci.R
 	resp := queryable.Query(req)
 	resp.Height = req.Height
 
-	if path[1] == "info" {
+	if path[1] == storeInfoPath {
 		commitInfo, err := app.cms.GetCommitInfoFromDB(resp.Height)
 		if err != nil {
 			return sdkerrors.QueryResult(err)
