ca8a42ef4a15f39ad513485147cac45c455a4d3d
Adam Tucker <adamleetucker@outlook.com>
2022-11-10 22:19:57 -0600

    add mvp cli

--- x/bank/client/cli/tx.go
+++ x/bank/client/cli/tx.go
@@ -1,6 +1,8 @@
 package cli
 
 import (
+	"strings"
+
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
@@ -21,6 +23,7 @@ func NewTxCmd() *cobra.Command {
 	}
 
 	txCmd.AddCommand(NewSendTxCmd())
+	txCmd.AddCommand(NewMultiSendTxCmd())
 
 	return txCmd
 }
@@ -48,6 +51,58 @@ ignored as it is implied from [from_key_or_address].`,
 				FromAddress: clientCtx.GetFromAddress().String(),
 				ToAddress:   args[1],
 				Amount:      coins,
+			}
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
+
+// NewMultiSendTxCmd returns a CLI command handler for creating a MsgSend transaction.
+func NewMultiSendTxCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use: "send [from_key_or_address] [to_address_csv] [amount_csv]",
+		Short: `Send funds from one account to many other accounts. Note, the'--from' flag is
+ignored as it is implied from [from_key_or_address].`,
+		Args: cobra.ExactArgs(3),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cmd.Flags().Set(flags.FlagFrom, args[0])
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			coins, err := sdk.ParseCoinsNormalized(args[2])
+			if err != nil {
+				return err
+			}
+
+			toAddresses := args[1]
+
+			toAddressesArray := strings.Split(toAddresses, ",")
+
+			input := types.Input{
+				Address: clientCtx.GetFromAddress().String(),
+			}
+			outputs := []types.Output{}
+
+			for i, coin := range coins {
+				outputs = append(outputs, types.Output{
+					Address: toAddressesArray[i],
+					Coins:   sdk.NewCoins(coin),
+				})
+			}
+
+			msg := &types.MsgMultiSend{
+				Inputs:  []types.Input{input},
+				Outputs: outputs,
 			}
 			if err := msg.ValidateBasic(); err != nil {
 				return err
