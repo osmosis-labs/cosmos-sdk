0ad987bc4c9075e749e01b3cf92306b58ad6d7a5
Dev Ojha <dojha@berkeley.edu>
2022-11-23 17:44:57 -0600

    More detail

--- store/cachekv2/writebuffer/README.md
+++ store/cachekv2/writebuffer/README.md
@@ -1,5 +1,15 @@
 # Write Buffer
 
-Write buffer is naive.
-Dead simple, map with a mutex for storing all edits within the height.
-Only unit of complexity is iterations.
+This module implements a simple write buffer, satisfying the current cosmos iteration API.
+We make a btree map of KV pairs that we write to.
+All reads are reads off the write buffer, this has no notion of parents.
+
+There are no mutexes here, its up to a caller to set concurrency guidelines.
+The API for reads and writes here, use `key` represented as a string.
+
+## Restrictions
+
+No new store writes are allowed while any iterator is open.
+(TODO: We need to relax this. TBD of do we this via data structure change, changing our iterator to know if a write happened and re-seek, etc. Common use cases to allow: Updating key being iterated on, e.g. `vec.itermut()`, or deleting a range of keys. (Dumb that we use this API for it, maybe we change that))
+
+TODO for future applications, investigate making tidwall/btree preserve allocated RAM on `Clear()`.
--- store/cachekv2/writebuffer/writebuffer.go
+++ store/cachekv2/writebuffer/writebuffer.go
@@ -10,109 +10,73 @@ import (
 )
 
 // Store wraps an in-memory cache around an underlying types.KVStore.
-type Store struct {
+type WriteBuffer struct {
 	// this is a map from key -> value, string -> []byte
 	// if value is nil, means its deleted.
-	bufferedWrites *btree.Map[string, []byte]
-	parent         types.KVStore
+	buffer *btree.Map[string, []byte]
 }
 
 // NewStore creates a new Store object
-func NewStore(parent types.KVStore) *Store {
-	return &Store{
-		bufferedWrites: btree.NewMap[string, []byte](16),
-		parent:         parent,
+func NewWriteBuffer() *WriteBuffer {
+	return &WriteBuffer{
+		buffer: btree.NewMap[string, []byte](16),
 	}
 }
 
-// GetStoreType implements Store.
-func (store *Store) GetStoreType() types.StoreType {
-	return store.parent.GetStoreType()
+func (wb *WriteBuffer) GetString(key string) (value []byte, found bool) {
+	value, found = wb.bufferedWrites.Get(key)
+	return value, found
 }
 
-func (store *Store) GetString(key string) (value []byte) {
-	value, _ = store.bufferedWrites.Get(key)
-	return value
+func (wb *WriteBuffer) SetString(key string, value []byte) {
+	wb.buffer.Set(key, value)
 }
 
-func (store *Store) SetString(key string, value []byte) {
-	store.bufferedWrites.Set(key, value)
-}
-
-func (store *Store) HasString(key string) bool {
-	_, found := store.bufferedWrites.Get(key)
+func (wb *WriteBuffer) HasString(key string) bool {
+	_, found := wb.buffer.Get(key)
 	return found
 }
 
-func (store *Store) DeleteString(key string) {
-	store.bufferedWrites.Delete(key)
+func (wb *WriteBuffer) DeleteString(key string) {
+	wb.buffer.Delete(key)
 }
 
 // Implements Cachetypes.KVStore.
-func (store *Store) Write() {
-	defer telemetry.MeasureSince(time.Now(), "store", "cachekv", "write")
+func (wb *WriteBuffer) WriteTo(parent types.KVStore) {
+	defer telemetry.MeasureSince(time.Now(), "store", "WriteBuffer", "write")
 
 	// TODO: Consider allowing usage of Batch, which would allow the write to
 	// at least happen atomically.
-	store.bufferedWrites.Ascend("", func(key string, value []byte) bool {
+	wb.buffer.Ascend("", func(key string, value []byte) bool {
 		if value == nil {
 			// We use []byte(key) instead of conv.UnsafeStrToBytes because we cannot
 			// be sure if the underlying store might do a save with the byteslice or
 			// not. Once we get confirmation that .Delete is guaranteed not to
 			// save the byteslice, then we can assume only a read-only copy is sufficient.
-			store.parent.Delete([]byte(key))
+			parent.Delete([]byte(key))
 			return false
 		}
 		// It already exists in the parent, hence delete it.
-		store.parent.Set([]byte(key), value)
+		parent.Set([]byte(key), value)
 		return false
 	})
 
 	// TODO: Investigate getting better memory re-use in this.
 	// Its a waste that we have to go re-allocate everything later :(
-	store.bufferedWrites.Clear()
+	wb.buffer.Clear()
 }
 
 //----------------------------------------
 // Iteration
 
 // Iterator implements types.KVStore.
-func (store *Store) Iterator(start, end []byte) types.Iterator {
+func (wb *WriteBuffer) Iterator(start, end []byte) types.Iterator {
 	var a types.Iterator
 	return a
 }
 
 // ReverseIterator implements types.KVStore.
-func (store *Store) ReverseIterator(start, end []byte) types.Iterator {
+func (wb *WriteBuffer) ReverseIterator(start, end []byte) types.Iterator {
 	var a types.Iterator
 	return a
 }
-
-// func (store *Store) iterator(start, end []byte, ascending bool) types.Iterator {
-// 	expectedId := nextIteratorId
-// 	fmt.Printf("calling lock for iterator creation, cur next id %v\n", expectedId)
-// 	fmt.Printf("current store mtx address %p\n", &store.mtx)
-// 	fmt.Printf("current memdb address %p\n", store.sortedCache)
-// 	store.mtx.Lock()
-// 	defer store.mtx.Unlock()
-// 	fmt.Printf("got past iterator lock with expected id %v, next iterator id %v\n", expectedId, nextIteratorId)
-
-// 	var parent, cache types.Iterator
-
-// 	fmt.Println("Creating a parent iterator")
-// 	if ascending {
-// 		parent = store.parent.Iterator(start, end)
-// 	} else {
-// 		parent = store.parent.ReverseIterator(start, end)
-// 	}
-// 	fmt.Println("Finished creating a parent iterator")
-
-// 	store.dirtyItems(start, end)
-
-// 	fmt.Println("Creating a mem iterator")
-// 	cache = newMemIterator(start, end, store.sortedCache, store.deleted, ascending)
-// 	fmt.Println("Finished creating a mem iterator")
-
-// 	iter := newCacheMergeIterator(parent, cache, ascending)
-// 	return iter
-// }
