320ca84099411cd2558c0558402179201ec21a95
Jacob Gadikian <jacobgadikian@gmail.com>
2022-12-18 00:20:55 +0700

    fix the handler tests

--- types/handler_test.go
+++ types/handler_test.go
@@ -4,36 +4,32 @@ import (
 	"testing"
 
 	"github.com/golang/mock/gomock"
-	"github.com/stretchr/testify/suite"
+	"github.com/stretchr/testify/require"
 
-	"github.com/cosmos/cosmos-sdk/tests/mocks"
+	"github.com/cosmos/cosmos-sdk/testutil/mock"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-type handlerTestSuite struct {
-	suite.Suite
-}
-
-func TestHandlerTestSuite(t *testing.T) {
-	suite.Run(t, new(handlerTestSuite))
-}
-
-func (s *handlerTestSuite) SetupSuite() {
-	s.T().Parallel()
-}
-
-func (s *handlerTestSuite) TestChainAnteDecorators() {
+func TestChainAnteDecorators(t *testing.T) {
 	// test panic
-	s.Require().Nil(sdk.ChainAnteDecorators([]sdk.AnteDecorator{}...))
+	require.Nil(t, sdk.ChainAnteDecorators([]sdk.AnteDecorator{}...))
 
 	ctx, tx := sdk.Context{}, sdk.Tx(nil)
-	mockCtrl := gomock.NewController(s.T())
-	mockAnteDecorator1 := mocks.NewMockAnteDecorator(mockCtrl)
+	mockCtrl := gomock.NewController(t)
+	mockAnteDecorator1 := mock.NewMockAnteDecorator(mockCtrl)
 	mockAnteDecorator1.EXPECT().AnteHandle(gomock.Eq(ctx), gomock.Eq(tx), true, gomock.Any()).Times(1)
-	sdk.ChainAnteDecorators(mockAnteDecorator1)(ctx, tx, true) //nolint:errcheck
+	_, err := sdk.ChainAnteDecorators(mockAnteDecorator1)(ctx, tx, true)
+	require.NoError(t, err)
 
-	mockAnteDecorator2 := mocks.NewMockAnteDecorator(mockCtrl)
-	mockAnteDecorator1.EXPECT().AnteHandle(gomock.Eq(ctx), gomock.Eq(tx), true, mockAnteDecorator2).Times(1)
-	mockAnteDecorator2.EXPECT().AnteHandle(gomock.Eq(ctx), gomock.Eq(tx), true, nil).Times(1)
-	sdk.ChainAnteDecorators(mockAnteDecorator1, mockAnteDecorator2)
+	mockAnteDecorator2 := mock.NewMockAnteDecorator(mockCtrl)
+	// NOTE: we can't check that mockAnteDecorator2 is passed as the last argument because
+	// ChainAnteDecorators wraps the decorators into closures, so each decorator is
+	// receiving a closure.
+	mockAnteDecorator1.EXPECT().AnteHandle(gomock.Eq(ctx), gomock.Eq(tx), true, gomock.Any()).Times(1)
+	mockAnteDecorator2.EXPECT().AnteHandle(gomock.Eq(ctx), gomock.Eq(tx), true, gomock.Any()).Times(1)
+
+	_, err = sdk.ChainAnteDecorators(
+		mockAnteDecorator1,
+		mockAnteDecorator2)(ctx, tx, true)
+	require.NoError(t, err)
 }
