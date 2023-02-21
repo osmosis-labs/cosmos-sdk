f8a7e528884358f5cd8314fabc92ec80d424b6a1
Roman <ackhtariev@gmail.com>
2023-02-21 17:34:01 -0500

    typo

--- baseapp/abci.go
+++ baseapp/abci.go
@@ -467,7 +467,7 @@ func (app *BaseApp) Query(req abci.RequestQuery) (res abci.ResponseQuery) {
 	}
 
 	if req.Path == "/cosmos.tx.v1beta1.Service/BroadcastTx" {
-		return dkerrors.QueryResultWithDebug(sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "can't route a broadcast tx message"), app.trace)
+		return sdkerrors.QueryResultWithDebug(sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "can't route a broadcast tx message"), app.trace)
 	}
 
 	// handle gRPC routes first rather than calling splitPath because '/' characters
