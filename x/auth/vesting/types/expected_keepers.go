5e5673472ca32a285a3f9ff5b0314264ef8afd4b
Adam Tucker <adamleetucker@outlook.com>
2023-10-29 21:25:35 -0600

    feat: add clawback vesting account (#178)

--- x/auth/vesting/types/expected_keepers.go
+++ x/auth/vesting/types/expected_keepers.go
@@ -6,10 +6,19 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
+// AccountKeeper defines the expected interface contract that is required by the
+// vesting module for storing accounts.
+type AccountKeeper interface {
+	GetAccount(context.Context, sdk.AccAddress) sdk.AccountI
+	SetAccount(context.Context, sdk.AccountI)
+}
+
 // BankKeeper defines the expected interface contract the vesting module requires
 // for creating vesting accounts with funds.
 type BankKeeper interface {
+	GetAllBalances(ctx context.Context, addr sdk.AccAddress) sdk.Coins
 	IsSendEnabledCoins(ctx context.Context, coins ...sdk.Coin) error
-	SendCoins(ctx context.Context, fromAddr, toAddr sdk.AccAddress, amt sdk.Coins) error
+	SendCoins(ctx context.Context, fromAddr sdk.AccAddress, toAddr sdk.AccAddress, amt sdk.Coins) error
+	SpendableCoins(ctx context.Context, addr sdk.AccAddress) sdk.Coins
 	BlockedAddr(addr sdk.AccAddress) bool
 }
