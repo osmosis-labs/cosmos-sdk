6cd13d63d3e5c076a1b0e81484dc7f4386fba23d
Jacob Gadikian <jacobgadikian@gmail.com>
2023-11-14 22:18:07 +0700

    account for not returning error from NewMutableTree and NewImmutableTree

--- store/cache/cache_test.go
+++ store/cache/cache_test.go
@@ -19,8 +19,7 @@ func TestGetOrSetStoreCache(t *testing.T) {
 	mngr := cache.NewCommitKVStoreCacheManager(cache.DefaultCommitKVStoreCacheSize)
 
 	sKey := types.NewKVStoreKey("test")
-	tree, err := iavl.NewMutableTree(db, 100, false)
-	require.NoError(t, err)
+	tree := iavl.NewMutableTree(db, 100, false)
 	store := iavlstore.UnsafeNewStore(tree)
 	store2 := mngr.GetStoreCache(sKey, store)
 
@@ -33,8 +32,7 @@ func TestUnwrap(t *testing.T) {
 	mngr := cache.NewCommitKVStoreCacheManager(cache.DefaultCommitKVStoreCacheSize)
 
 	sKey := types.NewKVStoreKey("test")
-	tree, err := iavl.NewMutableTree(db, 100, false)
-	require.NoError(t, err)
+	tree := iavl.NewMutableTree(db, 100, false)
 	store := iavlstore.UnsafeNewStore(tree)
 	_ = mngr.GetStoreCache(sKey, store)
 
@@ -47,8 +45,7 @@ func TestStoreCache(t *testing.T) {
 	mngr := cache.NewCommitKVStoreCacheManager(cache.DefaultCommitKVStoreCacheSize)
 
 	sKey := types.NewKVStoreKey("test")
-	tree, err := iavl.NewMutableTree(db, 100, false)
-	require.NoError(t, err)
+	tree := iavl.NewMutableTree(db, 100, false)
 	store := iavlstore.UnsafeNewStore(tree)
 	kvStore := mngr.GetStoreCache(sKey, store)
 
@@ -74,8 +71,7 @@ func TestReset(t *testing.T) {
 	mngr := cache.NewCommitKVStoreCacheManager(cache.DefaultCommitKVStoreCacheSize)
 
 	sKey := types.NewKVStoreKey("test")
-	tree, err := iavl.NewMutableTree(db, 100, false)
-	require.NoError(t, err)
+	tree := iavl.NewMutableTree(db, 100, false)
 	store := iavlstore.UnsafeNewStore(tree)
 	store2 := mngr.GetStoreCache(sKey, store)
 
@@ -95,8 +91,7 @@ func TestCacheWrap(t *testing.T) {
 	mngr := cache.NewCommitKVStoreCacheManager(cache.DefaultCommitKVStoreCacheSize)
 
 	sKey := types.NewKVStoreKey("test")
-	tree, err := iavl.NewMutableTree(db, 100, false)
-	require.NoError(t, err)
+	tree := iavl.NewMutableTree(db, 100, false)
 	store := iavlstore.UnsafeNewStore(tree)
 
 	cacheWrapper := mngr.GetStoreCache(sKey, store).CacheWrap()
