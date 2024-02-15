21ebd30bd2e88e0136b373de41c86643fa8a7f81
Adam Tucker <adamleetucker@outlook.com>
2024-02-15 10:27:32 -0700

    set distribution to each block for tests

--- x/distribution/abci.go
+++ x/distribution/abci.go
@@ -11,6 +11,8 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/distribution/types"
 )
 
+var BlockMultipleToDistributeRewards = int64(10)
+
 // BeginBlocker sets the proposer for determining distribution during endblock
 // and distribute rewards for the previous block.
 func BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock, k keeper.Keeper) {
@@ -20,8 +22,9 @@ func BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock, k keeper.Keeper) 
 	// ref https://github.com/cosmos/cosmos-sdk/issues/3095
 	blockHeight := ctx.BlockHeight()
 	// only allocate rewards if the block height is greater than 1
-	// and only allocate rewards for every multiple of 10 blocks for performance reasons.
-	if (blockHeight > 1 && blockHeight%10 == 0) || blockHeight == 2 {
+	// and for every multiple of 10 blocks for performance reasons.
+	// We special case blockHeight 2 to allow test cases to still be valid.
+	if (blockHeight > 1 && blockHeight%BlockMultipleToDistributeRewards == 0) || blockHeight == 2 {
 		// determine the total power signing the block
 		var previousTotalPower int64
 		for _, voteInfo := range req.LastCommitInfo.GetVotes() {
