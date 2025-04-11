5b4e03484cf88e7d1fce003ba75b4fdc5c81f15f
PaddyMc <paddymchale@hotmail.com>
2025-04-11 17:08:51 +0100

    fix: add missing check for key pagination (#643)
    * fix: add missing check for key pagination
    
    * chore: add change log.md

--- x/gov/keeper/collections_pagination_gov.go
+++ x/gov/keeper/collections_pagination_gov.go
@@ -300,6 +300,10 @@ func collFilteredPaginateByKey[K, V any, C query.Collection[K, V], T any](
 
 		kv, err := iterator.KeyValue()
 		if err != nil {
+			if strings.Contains(err.Error(), "no concrete type registered for type URL") {
+				// URL /osmosis.concentratedliquidity.v1beta1.CreateConcentratedLiquidityPoolsProposal
+				continue
+			}
 			return nil, nil, err
 		}
 		// if no predicate is specified then we just append the result
